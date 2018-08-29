61797d4ccd34e7e1b46c7d5a996402f518095797
vinodh vijayan <vinodh_vijayan@ymail.com>
2018-08-29 23:05:47 +0530

    My first file to commit

---
+++ githubexam/src/firstclass.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	driver.get("https://google.com");
+	}
+	}
