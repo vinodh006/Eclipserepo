4df35ccd0c7e9ae1fd8b6de5f4e3879cf7b05dd1
vinodh006 <42653759+vinodh006@users.noreply.github.com>
2018-08-29 23:24:11 +0530

    Update firstclass.java

--- githubexam/src/firstclass.java
+++ githubexam/src/firstclass.java
@@ -6,8 +6,6 @@ public class firstclass {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-	driver.get("https://google.com");
-		driver.close();
-		
+	driver.get("https://google.com");	
 	}
 	}
